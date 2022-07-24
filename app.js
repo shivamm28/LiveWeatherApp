6e86dbc468fc97233de5c4e89c61ffa8d7c960f8
shivamm28 <108068213+shivamm28@users.noreply.github.com>
2022-07-25 01:04:35 +0530

    update app.js

--- app.js
+++ app.js
@@ -42,6 +42,6 @@ app.post("/",function(req,res){
 
 
 
-app.listen("3000",function(){
-  console.log("server running on port 3000");
-});
+// app.listen("3000",function(){
+//   console.log("server running on port 3000");
+// });
